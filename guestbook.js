4a939a59fb129f25558847b7814c04c355a9c7e3
SUBODH KUMAR MISHRA <krishnasubodh@gmail.com>
2021-04-30 13:39:21 +0530

    Added API-route to guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://a1447648.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
